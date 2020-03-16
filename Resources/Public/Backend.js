4a327a20c9eb8af83e10321349ac6b06c471fd37
Jon Uhlmann <me@jonnitto.ch>
2020-03-16 09:41:48 +0100

    :lipstick: Render files

--- Resources/Public/Backend.js
+++ Resources/Public/Backend.js
@@ -1,1 +1,9 @@
-(function(){for(var e=["ContentModuleLoaded","PageLoaded","NodeCreated"],o=0;e.length>o;o++)document.addEventListener("Neos."+e[o],initJonnittoGoogleMaps)})();
+/*!
+ * Jonnitto.GoogleMaps - created by Jon Uhlmann
+ * @link https://github.com/jonnitto/Jonnitto.GoogleMaps
+ * Copyright 2016-2020 Jon Uhlmann
+ * Licensed under MIT
+ */
+
+!function(){"use strict";!function(){for(var e=["ContentModuleLoaded","PageLoaded","NodeCreated"],o=0;o<e.length;o++)document.addEventListener("Neos."+e[o],initJonnittoGoogleMaps)}()}();
+//# sourceMappingURL=Backend.js.map
