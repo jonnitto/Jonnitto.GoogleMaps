4e9ceb81546950f8a24dac5a35192bb32b197b0e
Jon Uhlmann <me@jonnitto.ch>
2021-08-18 00:16:50 +0200

    Upgrade: Build stack

--- Resources/Public/Backend.js
+++ Resources/Public/Backend.js
@@ -1,9 +1,2 @@
-/*!
- * Jonnitto.GoogleMaps - created by Jon Uhlmann
- * @link https://github.com/jonnitto/Jonnitto.GoogleMaps
- * Copyright 2016-2020 Jon Uhlmann
- * Licensed under MIT
- */
-
 !function(){"use strict";for(var e=["ContentModuleLoaded","PageLoaded","NodeCreated"],o=0;o<e.length;o++)document.addEventListener("Neos."+e[o],initJonnittoGoogleMaps)}();
 //# sourceMappingURL=Backend.js.map
